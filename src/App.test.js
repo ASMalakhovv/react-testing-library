c4db171f31e26957bc1025e6b3fe0f817ccae161
ASMalakhovv <alsmalahovv@mai.ru>
2023-03-10 10:11:14 +0600

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloWorldElem = screen.getByText(/hello world/i);
+  const btnElem = screen.getByRole('button');
+  const inputElem = screen.getByPlaceholderText(/inner text/i);
+  expect(helloWorldElem).toBeInTheDocument();
+  expect(btnElem).toBeInTheDocument();
+  expect(inputElem).toBeInTheDocument();
+  screen.debug()
+  expect(inputElem).toMatchSnapshot()
 });
